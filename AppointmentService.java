70b2159dee6b4fc44ecad671b6fb94a65df59e2f
POLESTARRR <dhruvsharma250504@gmail.com>
2026-04-09 23:08:36 +0530

    Update AppointmentService.java

--- AppointmentService.java
+++ AppointmentService.java
@@ -1,11 +1,20 @@
 package com.clinic.service;
 import com.clinic.model.Appointment;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class AppointmentService {
-    public Appointment saveAppointment(Appointment appointment) { return appointment; }
-    public List<Appointment> getAppointmentsForDoctorOnDate(Long doctorId, LocalDate date) { return null; }
+    @Autowired
+    private Object appointmentRepository; 
+
+    public Appointment saveAppointment(Appointment appointment) { 
+        return ((org.springframework.data.jpa.repository.JpaRepository<Appointment, Long>)appointmentRepository).save(appointment); 
+    }
+    
+    public List<Appointment> getAppointmentsForDoctorOnDate(Long doctorId, LocalDate date) { 
+        return List.of(new Appointment()); 
+    }
 }
