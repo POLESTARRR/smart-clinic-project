ba3644beadca841f5d314e48d9945e0c44395594
POLESTARRR <dhruvsharma250504@gmail.com>
2026-04-09 23:10:18 +0530

    Update PatientRepository.java

--- PatientRepository.java
+++ PatientRepository.java
@@ -2,7 +2,18 @@ package com.clinic.repository;
 import com.clinic.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for Patient entity.
+ */
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    
+    /**
+     * Retrieves a patient by their email address.
+     */
     Patient findByEmail(String email);
+    
+    /**
+     * Retrieves a patient using either their email address or phone number.
+     */
     Patient findByEmailOrPhoneNumber(String email, String phoneNumber);
 }
